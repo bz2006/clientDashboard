5bcb6def17d31486c59f059ae3e73c0fa29b425a
bezoft <bezoftwares@gmail.com>
2025-05-07 20:45:42 +0530

    cron jobs started

--- helpers/cronJobs.js
+++ helpers/cronJobs.js
@@ -1,20 +1,17 @@
 import cron from 'node-cron';
 import { deleteExpiredStaticMedia, deleteShortTrips, getExpiredUnitsDetails } from '../DBControllers/AutoJobs.js';
 
-// cron.schedule('*/30 * * * *', async () => {
-//   console.log("doing cron job");
-//   await deleteShortTrips()
-// });
+cron.schedule('*/30 * * * *', async () => {
+  console.log("doing cron job");
+  await deleteShortTrips()
+});
 
-// cron.schedule('30 0 * * *', async () => {
-//   console.log("doing cron expired job");
-//   const det=await getExpiredUnitsDetails()
-//   console.log(det);
-  
-// });
+cron.schedule('30 0 * * *', async () => {
+  const det=await getExpiredUnitsDetails();
+});
 
-// cron.schedule('*/2 * * * *', async () => {
-//   await deleteExpiredStaticMedia()
-// });
+cron.schedule('*/2 * * * *', async () => {
+  await deleteExpiredStaticMedia()
+});
 
 export default cron;
